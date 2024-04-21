a132d58d14b01087b157be334d66030263174760
anikmondol <anikmondol558363@gmil.com>
2024-04-21 17:56:26 +0600

    Template string,multiple line string, dynamic string

---
+++ template-string.js
@@ -0,0 +1,35 @@
+const first = "Jaan";
+const last = "pakhi";
+const greet = "Potas Potas";
+const name = first + " " + last + " " + greet;
+// console.log(name);
+
+
+const a = 10;
+const b = 20;
+
+const result = 'The sum of ' + a + ' and ' + b + " is " + (a + b);
+// console.log(result);
+
+
+const numbers = [22, 23, 88];
+const students = {
+    name : "anik",
+    age: 25
+}
+
+// const ans = `The sum of ${a} and ${b} is ${a + b}`;
+const ans = `The sum of ${numbers[1]} and ${students['age']} is ${numbers[1] + students['age']}`;
+console.log(ans);
+
+
+const email = `Hi john
+cena cena lage
+tumi mara mari korba naki`;
+// console.log(email);
+
+
+const challenge = `Johe Cena 
+cena naki ochena
+mair dimu prochur`;
+// console.log(challenge);
